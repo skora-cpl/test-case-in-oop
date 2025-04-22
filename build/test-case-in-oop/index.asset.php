dbb8afcb833d7eaf1ca3b844d3ea073136d97c4e
Will Skora <175735853+skora-cpl@users.noreply.github.com>
2025-04-22 14:24:30 -0400

    ran a build

--- build/test-case-in-oop/index.asset.php
+++ build/test-case-in-oop/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'c141e03a576687ef1e8a');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8883390f6386a58d6763');
